37452068ff3b8d513c3d9887b24f22aadea30e99
harshit <69914385+hxrsxit@users.noreply.github.com>
2022-07-12 17:20:35 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,9 +24,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = "0x!b#(1*cd73w$&azzc6p+essg7v=g80ls#z&xcx*mpemx&@9$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1",'hxt-blogs.herokuapp.com']
 
 
 # Application definition
